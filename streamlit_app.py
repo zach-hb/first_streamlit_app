5a20dde6e79793575eecd665ff64a8f10956d1be
Zachary Blackburn <126421928+zach-hb@users.noreply.github.com>
2023-12-13 12:53:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas as pd
-
+# restaurant menu
 streamlit.title('Our New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -10,6 +10,11 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+# import data from s3 bucket txt file
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+# pick list so user can pick fruit they want
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# display df on page
 streamlit.dataframe(my_fruit_list)
